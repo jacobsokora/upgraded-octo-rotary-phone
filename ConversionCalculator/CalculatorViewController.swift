27308568aa2ea2cd625bdd48cb6e18fc57509363
Jacob <jacobsokora@gmail.com>
2018-07-26 19:00:26 -0500

    Add final solution

--- ConversionCalculator/CalculatorViewController.swift
+++ ConversionCalculator/CalculatorViewController.swift
@@ -15,9 +15,11 @@ struct Converter {
 }
 
 class CalculatorViewController: UIViewController {
-
+    
     @IBOutlet weak var outputDisplay: UITextField!
     @IBOutlet weak var inputDisplay: UITextField!
+    
+    var inputText: String = ""
     
     let converters = [
         Converter(label: "fahrenheit to celcius", inputUnit: "°F", outputUnit: "°C"),
@@ -32,12 +34,12 @@ class CalculatorViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         setConverter(converters[0])
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     @IBAction func changeConverter(_ sender: UIButton) {
         let actionSheet = UIAlertController(title: "Choose converter", message: nil, preferredStyle: .actionSheet)
         for converter in converters {
@@ -50,8 +52,64 @@ class CalculatorViewController: UIViewController {
     
     func setConverter(_ converter: Converter) {
         currentConverter = converter
-        outputDisplay.text = converter.outputUnit
+        updateConversion()
+    }
+    
+    func updateConversion() {
+        guard let converter = currentConverter,
+            let value = Double(inputText) else {
+                return
+        }
+        inputDisplay.text = "\(inputText) \(converter.inputUnit)"
+        var convertedValue = value
+        switch converter.label {
+        case "fahrenheit to celcius":
+            convertedValue = (convertedValue - 32) * (5 / 9)
+        case "celcius to fahrenheit":
+            convertedValue = (convertedValue * (9 / 5)) + 32
+        case "miles to kilometers":
+            convertedValue = convertedValue * 1.60934
+        case "kilometers to miles":
+            convertedValue = convertedValue / 1.60934
+        default:
+            break
+        }
+        outputDisplay.text = "\(String(format: "%.2f", convertedValue)) \(converter.outputUnit)"
+    }
+    
+    @IBAction func clear(_ sender: UIButton) {
+        guard let converter = currentConverter else {
+            return
+        }
+        inputText = ""
         inputDisplay.text = converter.inputUnit
+        outputDisplay.text = converter.outputUnit
+    }
+    
+    @IBAction func invert(_ sender: UIButton) {
+        if inputText.contains("-") {
+            inputText.remove(at: inputText.startIndex)
+        } else {
+            inputText = "-" + inputText
+        }
+        updateConversion()
+    }
+    
+    @IBAction func append(_ sender: UIButton) {
+        if let period = inputText.index(of: ".") {
+            if inputText[inputText.index(after: period)...].count == 2 {
+                return
+            }
+        }
+        inputText += sender.titleLabel?.text ?? ""
+        updateConversion()
+    }
+    
+    @IBAction func decimal(_ sender: UIButton) {
+        if !inputText.contains(".") {
+            inputText += "."
+        }
+        updateConversion()
     }
 }
 
